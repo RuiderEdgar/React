4a037674edbed4ee747b304356318eb907aa7ccf
RuiderEdgar <edgar.riveraj@gmail.com>
2024-06-18 14:19:25 -0600

    seccion 11

--- hook-app/src/main.jsx
+++ hook-app/src/main.jsx
@@ -13,12 +13,13 @@ import ReactDOM from 'react-dom/client'
 // import { CallbackHook } from './06-memos/CallbackHook'
 // import { Padre } from './07-tarea-memo/Padre'
 // import './08-useReducer/intro-reducer.js'
+// import { TodoApp } from './08-useReducer/TodoApp.jsx'
 import './index.css'
-import { TodoApp } from './08-useReducer/TodoApp.jsx'
+import { MainApp } from './09-useContext/MainApp.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 //   <React.StrictMode>
-    <TodoApp/>
+    <MainApp/>
 //   </React.StrictMode>,
 )
